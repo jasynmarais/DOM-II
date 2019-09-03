d288d3dd62517063bd45bbf1c90ea3cf52be9625
Jasyn Marais <jasynmarais@gmail.com>
2019-09-03 15:25:17 +0200

    added mouseover

--- js/index.js
+++ js/index.js
@@ -1,2 +1,12 @@
 // Your code goes here
 
+// mouseover
+let magicbus = document.getElementById("funbus");
+
+magicbus.addEventListener("mouseover", event => {
+    event.target.style.visibility="hidden";
+    setTimeout(function () {
+        event.target.style.visibility = "visible";
+    }, 500);
+}, {once:true}
+);
